7c9db54b174074128fa616d0180d38b23fa2f954
sandysd7022 <129860565+sandysd7022@users.noreply.github.com>
2023-04-04 20:25:36 +0530

    Uploaded files

---
+++ SD/CategoryBackgroundColor/Setup/InstallData.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @category  Category Background Color
+ * @package   SixtySeven_CategoryBackgroundColor
+ * @author    SixtySeven
+ */
+
+namespace SixtySeven\CategoryBackgroundColor\Setup;
+
+use Magento\Framework\Setup\{
+    ModuleContextInterface,
+    ModuleDataSetupInterface,
+    InstallDataInterface
+};
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory) {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'background_colour', [
+            'type'     => 'varchar',
+            'label'    => 'Background Colour',
+            'input'    => 'text',
+            'source'   => '',
+            'visible'  => true,
+            'default'  => '0',
+            'required' => false,
+            'global'   => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            'group'    => 'Display Settings',
+        ]);
+    }
+}
---
+++ SD/CategoryBackgroundColor/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category  Category Background Color
+ * @package   SixtySeven_CategoryBackgroundColor
+ * @author    SixtySeven
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SixtySeven_CategoryBackgroundColor',
+    __DIR__
+);
